bfc20ebc9a889d3a2fb0c32d72ab157276d1c2aa
Rozaan3309 <Rozaan3309@users.noreply.github.com>
2020-04-07 16:10:24 +0700

    callback multiple file Done

--- index.js
+++ index.js
@@ -10,8 +10,26 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+ fs.readFile(parent_file, 'utf8', (err, data1) =>{
+   sleep(5000);
+    parents = JSON.parse(data1)
+    fs.readFile(children_file, 'utf8', (err, data2) =>{
+      sleep(5000);
+      children = JSON.parse(data2)
+      for(let a = 0; a < parents.length; a++){
+        let temp = []
+        for(let b = 0; b < children.length; b++){
+          if(parents[a].last_name === children[b].family){
+            temp.push(children[b].full_name)
+          }
+        }
+        parents[a].children = temp
+      }
+      console.log(parents)
+    });
+  });
 }
 
 match_data('./parents.json', './children.json')
 console.log("Notification : Data sedang diproses !");
+
