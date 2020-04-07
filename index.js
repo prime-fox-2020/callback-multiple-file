3f689f3c854024bca35d373bd6fe173834b8e0fd
chabode <rizqa.aulia@gmail.com>
2020-04-07 15:01:39 +0700

    Callback Multiple Files : OK

--- index.js
+++ index.js
@@ -11,6 +11,33 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file, 'utf-8',(err, parentDatas) => {
+    sleep(5000)
+    console.log('Generating Parents Data')
+    if (err){
+      throw err
+    } else {
+      let parentData = JSON.parse(parentDatas)
+      fs.readFile(children_file, 'utf-8', (err, childrenDatas) => {
+        sleep(5000)
+        console.log('Generating Children Data')
+        if (err){
+          throw err
+        } else {
+          let childrenData = JSON.parse(childrenDatas)
+          for (let i = 0; i<parentData.length; i++){
+            parentData[i].children = []
+            for (let j = 0; j<childrenData.length; j++){
+              if (parentData[i].last_name === childrenData[j].family){
+                parentData[i].children.push(childrenData[j].full_name)
+              }
+            }
+          }
+          console.log(parentData)
+        }
+      })
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
