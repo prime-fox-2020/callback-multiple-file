778b548193ee820009e69269aaf16152abd73fc5
syukur <syukurbaru@gmail.com>
2020-04-07 17:09:13 +0700

    add callback multiple file

--- index.js
+++ index.js
@@ -11,6 +11,24 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file, 'utf8', function (err, parent) {
+    fs.readFile(children_file, 'utf8', function (err, children) {
+      let parent_data = JSON.parse(parent)
+      let children_data = JSON.parse(children)
+
+      for (let i = 0; i < parent_data.length; i++) {
+        parent_data[i].children = []
+        for (let j = 0; j < children_data.length; j++) {
+          // jika last name parent sama dengan family name children
+          if (parent_data[i].last_name === children_data[j].family) {
+            parent_data[i].children.push(children_data[j].full_name)
+          }
+        }
+      }
+      sleep(5000)
+      console.log(parent_data);
+    })
+  })
 }
 
 match_data('./parents.json', './children.json')
