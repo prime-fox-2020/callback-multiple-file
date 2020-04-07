3f8d75b60db75b2ac320260859ffab281ea59370
Muhammad Risky Nugraha <muhammadrisky83@gmail.com>
2020-04-07 15:32:18 +0700

    fix async

--- index.js
+++ index.js
@@ -10,25 +10,32 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  fs.readFile(parent_file, null, (err,data) =>{
-    if(err) throw err
-    sleep(3000)
-    let parent = JSON.parse(data)
-    let children = JSON.parse(fs.readFileSync(children_file, 'utf8'))
+  fs.readFile(parent_file, (err,data) =>{
+    sleep(3000);
+    console.log('Sedang baca file parent...');
+    sleep(3000);
+    if(err) throw err;
+    let parent = JSON.parse(data);
+    console.log('Sedang baca file children...');
+    sleep(3000);
 
-    for (let i = 0; i < parent.length; i++) {
-      parent[i].children = []
-      for (let j = 0; j < children.length; j++) {
-        if(parent[i].last_name == children[j].family){
-          parent[i].children.push(children[j].full_name)
+    fs.readFile(children_file, (err, data) => {
+      if(err) throw err;
+      let children = JSON.parse(data);
+      for (let i = 0; i < parent.length; i++) {
+        parent[i].children = [];
+        for (let j = 0; j < children.length; j++) {
+          if(parent[i].last_name == children[j].family){
+            parent[i].children.push(children[j].full_name);
+          }
         }
       }
-    }
-    console.log(parent)
+      console.log(parent);
+    })
   })
 }
 
 
 
-match_data('./parents.json', './children.json')
+match_data('./parents.json', './children.json');
 console.log("Notification : Data sedang diproses !");
