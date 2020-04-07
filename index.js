ea44cfa2ec96dcc4f39c7e53086c5565e0778c6f
andreanwirapradana <61081321+andreanwirapradana@users.noreply.github.com>
2020-04-07 19:32:18 +0700

    tugas callback multiple file

--- index.js
+++ index.js
@@ -11,6 +11,34 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  const parents = fs.readFile('./parents.json', 'utf8', (err, data) => {
+    if (err) {
+      console.log(err.message)
+    }
+    else {
+      // console.log(JSON.parse(data))
+      const parentsList = JSON.parse(data)
+      const children = fs.readFile('./children.json', 'utf8', (err, data) => {
+        if (err) {
+          console.log(err.message);
+        }
+        else {
+          // console.log(JSON.parse(data));
+          const childrenList = JSON.parse(data)
+          for (let i = 0; i < parentsList.length; i++) {
+            parentsList[i].children = []
+            for (let j = 0; j < childrenList.length; j++) {
+              // console.log(parentsList.length, childrenList.length);
+              if (parentsList[i].last_name === childrenList[j].family) {
+                parentsList[i].children.push(childrenList[j].full_name);
+              }
+            }
+          }
+          console.log(parentsList);
+        }
+      })
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
