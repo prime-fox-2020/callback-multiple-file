5903d853c5e298588df4360ac57d520fe43e046e
rezarr-dev <rezarr.business@gmail.com>
2020-04-08 00:53:09 +0700

    callbacl multiple done

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+	fs.readFile(parent_file, 'utf8', (err, data) => {
+		if (err) {
+			throw err
+		} else {
+			let parent = JSON.parse(data)
+			fs.readFile(children_file, 'utf8', (err, data) => {
+				if (err) {
+					throw err
+				} else {
+					let child = JSON.parse(data)
+					for (let i = 0; i < parent.length; i++) {
+						parent[i].children = []
+						for (let j = 0; j < child.length; j++) {
+							if (child[j].family === parent[i].last_name) {
+								parent[i].children.push(child[j].full_name)
+							}
+						}
+          }
+          sleep(5000)
+					console.log(parent)
+				}
+			})
+		}
+	})
 }
 
 match_data('./parents.json', './children.json')
