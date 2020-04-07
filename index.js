c64578c78655480f1741aaf5c5b3e8b25a6559e9
akmaliavitri <akmaliavitri21@gmail.com>
2020-04-07 16:37:50 +0700

    readFile

--- index.js
+++ index.js
@@ -9,9 +9,34 @@ function sleep(milliseconds) {
   }
 }
 
+
 function match_data(parent_file, children_file) {
-  // Code here
+
+  fs.readFile(parent_file, null, (err,data) =>{
+    if(err) {
+      throw (err)
+    } else {
+
+      let parents = JSON.parse(data)
+
+      let children = fs.readFileSync(children_file, 'utf8')
+      let chil = JSON.parse(children)
+
+      for(let i=0; i<parents.length; i++){
+        parents[i].chil = []
+        for(let j=0; j<chil.length; j++) {
+          if(parents[i].last_name === chil[j].family){
+            parents[i].chil.push(chil[j].full_name)
+          }
+        }
+      }
+
+      console.log(parents)
+    }
+    
+  })
 }
+
 
 match_data('./parents.json', './children.json')
 console.log("Notification : Data sedang diproses !");
