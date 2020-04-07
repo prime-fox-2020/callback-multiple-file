cc22a49e3f96603d35b95f9c5e9ed2e9b6bf4219
Samirudin242 <samtheyaku@gmail.com>
2020-04-07 18:49:32 +0700

    done

--- index.js
+++ index.js
@@ -9,8 +9,30 @@ function sleep(milliseconds) {
   }
 }
 
-function match_data(parent_file, children_file) {
-  // Code here
+
+function match_data (parent_file, children_file) {
+  fs.readFile(parent_file, (err, parentsData) => {
+    if (err) throw err
+    sleep(5000)
+    const parentArr = JSON.parse(parentsData)
+    fs.readFile(children_file, (err, childrenData) => {
+      if (err) throw err
+      sleep(5000)
+      const childArr = JSON.parse(childrenData)
+      parentArr.forEach(parents => {
+        childArr.forEach(childrens => {
+          if (parents['last_name'] === childrens['family']) {
+            if (parents['children'] === undefined) {
+              parents['children'] = [childrens['full_name']]
+            } else {
+              parents['children'].push(childrens['full_name'])
+            }
+          }
+        })
+      })
+      console.log(parentArr)
+    })
+  })
 }
 
 match_data('./parents.json', './children.json')
