a1bd54b3d1403f9bc58d629fb272a9951be9eda2
unknown <sonymartha4@gmail.com>
2020-04-07 23:03:46 +0700

    fix

--- index.js
+++ index.js
@@ -9,9 +9,40 @@ function sleep(milliseconds) {
   }
 }
 
-function match_data(parent_file, children_file) {
-  // Code here
+function match_data(parentList, childrenList) {
+  let parents, childrens
+  fs.readFile(parentList, function(err, data) {
+    sleep(5000)
+    parents = JSON.parse(data)
+    fs.readFile(childrenList, function(err, data) {
+      sleep(5000)
+      childrens = JSON.parse(data)
+      for (let i = 0; i < parents.length; i++) {
+        parents[i].childrens = []
+        for (let j = 0; j < childrens.length; j++) {
+          if (parents[i].last_name === childrens[j].family) {
+            parents[i].childrens.push(childrens[j].full_name)
+          }
+        }
+      }
+      console.log(parents);
+    });
+  });
+  
+}
+
+function clearScreen () {
+  // Un-comment this line if you have trouble with console.clear();
+  // return process.stdout.write('\033c');
+  console.clear();
 }
 
 match_data('./parents.json', './children.json')
-console.log("Notification : Data sedang diproses !");
+
+let a1 = '.'
+for(let a = 0 ; a < 4; a++){
+  sleep(1000)
+  clearScreen()
+  console.log(`Notification : Data sedang diproses ${a1}`);
+  a1 += ' .'
+}
