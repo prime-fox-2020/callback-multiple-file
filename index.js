8441c6091f846bef694006c8881af41a22172563
alamrumambi <alam.abumaryam@gmail.com>
2020-04-07 10:52:28 -0400

    callback-multiple-file

--- index.js
+++ index.js
@@ -10,8 +10,31 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+  let parent, children;
+  fs.readFile(parent_file, 'utf8', (err, data) => {
+    if (err) throw err;
+    else {
+      sleep(5000);
+      parent = JSON.parse(data);
+      fs.readFile(children_file, 'utf8', (err, data) => {
+        if (err) throw err;
+        else {
+          sleep(5000);
+          children = JSON.parse(data);
+          for (let i in parent){
+            for (let j in children){
+              if (children[j].family == parent[i].last_name){
+                if (!parent[i]['children']) parent[i]['children'] = [children[j].full_name];
+                else parent[i]['children'].push(children[j].full_name);
+              }
+            }
+          }
+          console.log(parent);
+        }
+      });
+    }
+  });
 }
 
-match_data('./parents.json', './children.json')
 console.log("Notification : Data sedang diproses !");
+match_data('./parents.json', './children.json')
