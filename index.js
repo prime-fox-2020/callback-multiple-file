b637a6c612a2c5468dc98197eb6c4a6004375f39
charles2901 <chrlsjnthn@gmail.com>
2020-04-07 12:57:54 +0700

    solve

--- index.js
+++ index.js
@@ -10,7 +10,33 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+  fs.readFile(parent_file, 'utf8', (err,data) => {
+    sleep(5000)
+    if(err){
+      throw err
+    }
+    else{
+      let parent_data = JSON.parse(data)
+      fs.readFile(children_file, 'utf8', (e, d) => {
+        sleep(5000)
+        if(e){
+          throw e
+        }
+        else{
+          let children_data = JSON.parse(d)
+          for(let i = 0; i < parent_data.length; i++){
+            parent_data[i].children = []
+            for(let j = 0; j < children_data.length; j++){
+              if(parent_data[i].last_name === children_data[j].family){
+                parent_data[i].children.push(children_data[j].full_name)
+              }
+            }
+          }
+          console.log(parent_data)
+        }
+      })
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
