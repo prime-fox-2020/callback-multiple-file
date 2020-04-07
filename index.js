169c63bc84ad867361523282aebd62cb1e1a3dda
vinita-dewi <61119004+vinita-dewi@users.noreply.github.com>
2020-04-07 23:45:30 +0700

    finished multiplefile

--- index.js
+++ index.js
@@ -10,8 +10,38 @@ function sleep(milliseconds) {
 }
 
 function match_data(parent_file, children_file) {
-  // Code here
+  console.log(`Processing Parents Data ...`)
+  sleep(5000)
+  fs.readFile(parent_file, 'utf8', (err, parentFile) => {
+    if (err) {
+      throw err;
+    } else {
+      let parentData = JSON.parse(parentFile)
+      console.log(`Processing Childrens Data ...`)
+      sleep(5000)
+      fs.readFile(children_file,'utf8',(err,childFile) =>{
+        if(err){
+          throw err;
+        }else{
+          console.log(`Finding Matching Data ... `)
+          sleep(5000)
+          let childData = JSON.parse(childFile)
+          for(var i = 0; i<parentData.length;i++){
+            parentData[i].children = []
+            for(var j = 0 ; j<childData.length;j++){
+              if(parentData[i].last_name === childData[j].family){
+                parentData[i].children.push(childData[j].full_name)
+              }
+            }
+          }
+        }
+        console.log(`Showing Finished Data ... `)
+        sleep(500)
+        console.log(parentData)
+      })
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
-console.log("Notification : Data sedang diproses !");
+// console.log("Notification : Data sedang diproses !");
