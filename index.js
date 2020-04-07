d5242fa9086d14da64f07a34fcaf1affe5bb632b
zuhdi <zudhy.duddy@gmail.com>
2020-04-07 19:50:42 +0700

    done multiple file

--- index.js
+++ index.js
@@ -11,6 +11,31 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  let parent = []
+  let children = []
+  fs.readFile(parent_file, 'utf8', (err, data) => {
+    sleep(5000)
+    if (err){
+      console.log('error :' + err.message)
+    } else {
+      parent = JSON.parse(data)
+      fs.readFile(children_file, 'utf8', (err, data) => {
+        if (err){
+          console.log('error :' + err.message)
+        } else {
+          children = JSON.parse(data)
+          for (let i = 0; i < parent.length; i++) {
+            parent[i].children = []
+            for (let j = 0; j < children.length; j++) {
+              if (parent[i].last_name === children[j].family)
+              parent[i].children.push(children[j].full_name)
+            }
+          }
+        }
+        console.log(parent)
+      })
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
