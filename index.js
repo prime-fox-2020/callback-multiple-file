6a86be93a6be9108d9b16ed0fbf2e439b8f39d27
Xaber <getar.metal@gmail.com>
2020-04-07 22:43:16 +0700

    first commit, callback multiple file

--- index.js
+++ index.js
@@ -11,6 +11,34 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  let parent, children;
+  fs.readFile(parent_file, 'utf8', (err, parentData) => {
+    if (err) {
+      console.log(err);
+    } else {
+      parent = JSON.parse(parentData);
+      sleep(5000);
+    }
+
+    fs.readFile(children_file, 'utf8', (err, childrenData) => {
+      if (err) {
+        console.log(err);
+      } else {
+        children = JSON.parse(childrenData);
+        sleep(5000);
+      }
+
+      for (let a = 0; a < parent.length; a++) {
+        parent[a].children = [];
+        for (let b = 0; b < children.length; b++) {
+          if (children[b].family == parent[a].last_name) {
+            parent[a].children.push(children[b].full_name);
+          }
+        }
+      }
+      console.log(parent);
+    })
+  })
 }
 
 match_data('./parents.json', './children.json')
