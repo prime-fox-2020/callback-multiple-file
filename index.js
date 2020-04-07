33e130ed6a245484137cc87677384903dad94cf9
susilo hartomo <encusilo545110@gmail.com>
2020-04-07 15:10:31 +0700

    callback read data

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile(parent_file, "utf-8", ((err, data) => {
+    if (err) {
+      throw err
+    } else {
+      let parent_data = JSON.parse(data)
+      fs.readFile(children_file, 'utf-8', ((err, data) => {
+        if (err) {
+          throw err
+        } else {
+          let children_data = JSON.parse(data)
+          for (let i = 0; i < parent_data.length; i++) {
+            for (let j = 0; j < children_data.length; j++) {
+              if (parent_data[i].children == undefined) {
+                parent_data[i].children = []
+              }
+              if (parent_data[i].last_name == children_data[j].family) {
+                parent_data[i].children.push(children_data[j].full_name)
+              }
+            }
+          }
+          console.clear();
+          console.log(parent_data);
+          sleep(5000)
+        }
+      }))
+      sleep(5000)
+    }
+    sleep(5000)
+  }))
 }
 
 match_data('./parents.json', './children.json')
