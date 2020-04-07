3722d63516e29758ba423b185746f0bba212e072
Muhammad Yusuf <muhammad_yusuf22@outlook.co.id>
2020-04-07 08:34:40 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const fs = require('fs');
+
+function sleep(milliseconds) {
+  var start = new Date().getTime();
+  for (var i = 0; i < 1e7; i++) {
+    if ((new Date().getTime() - start) > milliseconds) {
+      break;
+    }
+  }
+}
+
+function match_data(parent_file, children_file) {
+  // Code here
+}
+
+match_data('./parents.json', './children.json')
+console.log("Notification : Data sedang diproses !");
