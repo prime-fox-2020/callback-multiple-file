206a8b0adaaafe6f3ec0290981783893d52f7525
Zetra416 <penguin_vs_yeti@yahoo.com>
2020-04-07 18:44:56 +0700

    finished file index.js

--- index.js
+++ index.js
@@ -11,6 +11,35 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  fs.readFile("parents.json","utf8", (err, dataP) => {
+    if (err) {
+      console.log(err);
+    }
+    else {
+      let parents_data = JSON.parse(dataP);
+      // console.log(parents_data);
+      sleep(5000);
+      fs.readFile("children.json", "utf8", (err, dataC) => {
+        if (err) {
+          console.log(err);
+        }
+        else {
+          let children = JSON.parse(dataC);
+          // console.log(children);
+          sleep(5000);
+          for (let i = 0; i < parents_data.length; i++) {
+            parents_data[i].children = [];
+            for (let j = 0; j < children.length; j++) {
+              if (parents_data[i].last_name === children[j].family) {
+                parents_data[i].children.push(children[j].full_name);
+              }
+            }
+          }
+        }
+        console.log(parents_data);
+      });
+    }
+  })
 }
 
 match_data('./parents.json', './children.json')
