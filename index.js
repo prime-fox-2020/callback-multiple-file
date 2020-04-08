6ff3bca78ca41e77c07073efa44e59ac19b2cd9b
Vincent Guizot <30890039+CenZo128@users.noreply.github.com>
2020-04-08 09:33:29 +0700

    Multiple Files

--- index.js
+++ index.js
@@ -11,6 +11,30 @@ function sleep(milliseconds) {
 
 function match_data(parent_file, children_file) {
   // Code here
+  let parentList, childrenList;
+  fs.readFile(parent_file, 'utf-8', (err,data) => { 
+    sleep(5000)
+    if (err) throw err;
+    parentList = JSON.parse(data);
+    fs.readFile(children_file, 'utf-8', (err,data) => { 
+      sleep(5000)
+      if (err) throw err
+      childrenList = JSON.parse(data);
+      for(let i=0; i<parentList.length; i++){
+        parentList[i]['children'] = [];
+        for(let j=0; j<childrenList.length; j++){
+          if(parentList[i].last_name == childrenList[j].family){
+            parentList[i].children.push(childrenList[j].full_name);
+          }
+        }
+      }
+      console.log(parentList);
+
+    })
+
+    return data
+  })
+
 }
 
 match_data('./parents.json', './children.json')
