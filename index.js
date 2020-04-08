ca70abd6e9a0a21704a761b71ee6c29e8c7d70fc
riolly <albert.r.cahyantara@gmail.com>
2020-04-08 12:54:17 +0700

    Asynchronous read file using callback

--- index.js
+++ index.js
@@ -2,33 +2,46 @@ const fs = require('fs');
 
 function sleep(milliseconds) {
   var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds) {
-      break;
-    }
+  while ((new Date().getTime() - start) < milliseconds) {
+    // Hold the process. Do nothing.
   }
 }
 
 
-function match_data(parent_file, children_file, callback) {
-  sleep(5000);
-  const dataParent = JSON.parse(fs.readFileSync(parent_file, 'utf8'));
-  const dataChildren = JSON.parse(fs.readFileSync(children_file, 'utf8'));
-
-  for (let parent of dataParent) {
-    sleep(5000);
-    parent.childrens = [];
-    for (let children of dataChildren) {
-      if (parent.last_name == children.family) {
-        parent.childrens.push(children.full_name);
-      }
+function match_data(parent_file, children_file) {
+  console.log("Notification: Data sedang diproses!");
+  fs.readFile(parent_file, 'utf8', (err, data) => {
+    if (err) {
+      throw err;
+    } else {  
+      const dataParent = JSON.parse(data);
+      sleep(dataParent.length * 200);
+      console.log(`Selesai membaca data parent.`);
+      fs.readFile(children_file, 'utf8', (err, data) => {
+        if (err) {
+          throw err;
+        } else {
+          const dataChildren = JSON.parse(data);
+          sleep(dataChildren.length * 100);
+          console.log(`Selesai membaca data children.`);
+          console.log(`\nMemulai proses macthing.`);
+          sleep(1000);
+          for (let parent of dataParent) {
+            parent.children = [];
+            for (let children of dataChildren) {
+              if (children.family == parent.last_name)
+                parent.children.push(children.full_name)
+            }
+            sleep(parent.children.length*100);
+            console.log(`\nProses matching berhasil.`);
+            console.log(parent);
+          }
+          console.log(`\nProses matching selesai.`);
+        }
+      }); 
     }
-    callback(parent);
-  }
+  });
 }
 
-
-
-console.log("Notification : Data sedang diproses !");
-match_data('./parents.json', './children.json', (parent) => {console.log(parent)});
+match_data('./parents.json', './children.json');
   
